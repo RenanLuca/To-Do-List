bffdcebb631686da49560da5d2a469bd7b8fde3d
Renan de Luca <Renan de Luca>
2023-09-18 17:54:13 -0300

    update src img

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css'
 export function Header() {
     return (
         <header className={styles.headerMain}>
-            <img className={styles.headerLogo} src="../assets/logo.svg" alt="Logo toDoList" />
+            <img className={styles.headerLogo} src="./assets/logo.svg" alt="Logo toDoList" />
         </header>
     )
 }
